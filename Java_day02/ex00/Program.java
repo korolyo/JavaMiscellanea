5cc8c18d399068b4dc75ac2bbf20afcc01601c1b
Korolyo <Korolyo@gmail.com>
2022-04-20 19:44:24 +0300

    D02ex00

--- Java_day02/ex00/Program.java
+++ Java_day02/ex00/Program.java
@@ -1,9 +1,42 @@
 import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.*;
 
 public class Program {
-    public static void main(String[] args) {
-        well...
+
+    public static Map<String, String> makeMapFromSignatures(String signatureFilePath) throws IOException {
+
+        Map<String, String> res = new HashMap<String, String>();
+
+        try {
+            FileInputStream br = new FileInputStream(new File(signatureFilePath));
+            int line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = br.read()) != -1) {
+                String[] data = builder.toString().split(", ");
+                res.put(data[1], data[0]);
+                System.out.println("kew: " + data[1] + " vawue: " + data[0]);
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Exception");
+        }
+        return res;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> signatures = new HashMap<String, String>();
+        String resultFileName = new String("result.txt");
+        String signatureFilePath = new String("signatures.txt");
+
+        File result = new File(resultFileName);
+        if (result.createNewFile())
+            System.out.println("FIle created");
+        Scanner filePath = new Scanner(System.in);
+
+        signatures = makeMapFromSignatures(signatureFilePath);
 
     }
 }
