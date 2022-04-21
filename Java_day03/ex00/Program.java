bcbeddf9bd0fdd3a1a1eb490d2c9def37efe98a9
korolyo <korolyo@gmail.com>
2022-04-21 20:11:55 +0300

    Day03

--- Java_day03/ex00/Program.java
+++ Java_day03/ex00/Program.java
@@ -16,7 +16,44 @@ public class Program  implements Runnable{
             System.exit(-1);
         }
 
-        Thread thread
+        Thread thread1 = new Thread(new Program(cycleCount, "Egg"));
+        Thread thread2 = new Thread(new Program(cycleCount, "Hen"));
 
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        new Program(cycleCount,"Human").run();
+    }
+
+    private static boolean validate(String[] args) {
+        if (args.length != 1 || !args[0].startsWith("--count=")) {
+            return false;
+        }
+        String[] counts = args[0].split("=");
+        if (counts.length != 2) {
+            return false;
+        }
+        try {
+            cycleCount = Integer.parseInt(counts[1]);
+        } catch (Exception e) {
+            return false;
+        }
+        return cycleCount > 0;
+    }
+
+
+    public Program(int count, String message) {
+        this.count = count;
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(message);
+        }
     }
 }
