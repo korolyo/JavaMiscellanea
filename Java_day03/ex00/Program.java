ded49a52a14697cbcd1988ddb75008209ca1f7ae
korolyo <korolyo@gmail.com>
2022-04-22 21:32:15 +0300

    Day03

--- Java_day03/ex00/Program.java
+++ Java_day03/ex00/Program.java
@@ -1,23 +1,28 @@
-import java.util.*;
-import java.io.*;
-
-public class Program  implements Runnable{
+public class Program  implements Runnable {
 
     private int count;
     private String message;
 
-    private static int cycleCount;
-    private static final String USAGE = "usage:/njava Program --count=[int>0]";
+    private static int countNum;
+    private static final String USAGE = "Try again:/njava Program --count=[int>0]";
 
     public static void main(String[] args) throws InterruptedException {
 
-        if (!validate(args)) {
+        if (args.length != 1 || !args[0].startsWith("--count=")) {
             System.out.println(USAGE);
             System.exit(-1);
         }
 
-        Thread thread1 = new Thread(new Program(cycleCount, "Egg"));
-        Thread thread2 = new Thread(new Program(cycleCount, "Hen"));
+        String[] countStr = args[0].split("=");
+        if (countStr.length != 2) {
+            System.out.println(USAGE);
+            System.exit(-1);
+        }
+
+        countNum = Integer.parseInt(countStr[1]);
+
+        Thread thread1 = new Thread(new Program(countNum, "Egg"));
+        Thread thread2 = new Thread(new Program(countNum, "Hen"));
 
         thread1.start();
         thread2.start();
@@ -25,25 +30,8 @@ public class Program  implements Runnable{
         thread1.join();
         thread2.join();
 
-        new Program(cycleCount,"Human").run();
+        new Program(countNum,"Human").run();
     }
-
-    private static boolean validate(String[] args) {
-        if (args.length != 1 || !args[0].startsWith("--count=")) {
-            return false;
-        }
-        String[] counts = args[0].split("=");
-        if (counts.length != 2) {
-            return false;
-        }
-        try {
-            cycleCount = Integer.parseInt(counts[1]);
-        } catch (Exception e) {
-            return false;
-        }
-        return cycleCount > 0;
-    }
-
 
     public Program(int count, String message) {
         this.count = count;
--- Java_day03/ex01/Program.java
+++ Java_day03/ex01/Program.java
@@ -1,45 +1,35 @@
-import java.util.*;
-import java.io.*;
-
 public class Program implements Runnable {
+
     private int count;
     private String message;
 
-    private static String key = "Hen";
-    private static int cycleCount;
-    private static final String USAGE = "usage:\njava Program --count=[int > 0]";
+    private static String word = new String("Hen");
+    private static int countNum;
+    private static final String USAGE = "Try again:/njava Program --count=[int>0]";
 
     public static void main(String[] args) throws InterruptedException {
 
-        if (!validate(args)) {
+        if (args.length != 1 || !args[0].startsWith("--count=")) {
             System.out.println(USAGE);
             System.exit(-1);
         }
 
-        Thread thread1 = new Thread(new Program(cycleCount, "Egg"));
-        Thread thread2 = new Thread(new Program(cycleCount, "Hen"));
+        String[] countStr = args[0].split("=");
+        if (countStr.length != 2) {
+            System.out.println(USAGE);
+            System.exit(-1);
+        }
+
+        countNum = Integer.parseInt(countStr[1]);
+
+        Thread thread1 = new Thread(new Program(countNum, "Egg"));
+        Thread thread2 = new Thread(new Program(countNum, "Hen"));
 
         thread1.start();
         thread2.start();
 
         thread1.join();
         thread2.join();
-    }
-
-    private static boolean validate(String[] args) {
-        if (args.length != 1 || !args[0].startsWith("--count=")) {
-            return false;
-        }
-        String[] counts = args[0].split("=");
-        if (counts.length != 2) {
-            return false;
-        }
-        try {
-            cycleCount = Integer.parseInt(counts[1]);
-        } catch (Exception e) {
-            return false;
-        }
-        return cycleCount > 0;
     }
 
     public Program(int count, String message) {
@@ -52,9 +42,9 @@ public class Program implements Runnable {
         int i = 0;
         while (i < count) {
             synchronized (Program.class) {
-                if (!key.equals(message)) {
+                if (!word.equals(message)) {
                     System.out.println(message);
-                    key = message;
+                    word = message;
                     i++;
                 }
             }
